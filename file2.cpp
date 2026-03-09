88d54ef5bbbba1bf739dc63e61fc9b92a70414d8
scrollshadow <zaldhypramana@gmail.com>
2026-03-09 15:58:06 +0700

    1

---
+++ file2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+float Rata_Rata(float a, float b){
+    return (a+b/2);
+}
+
+string Status_Lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "gagal";
+}
+
